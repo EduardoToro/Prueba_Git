994d34a3c3ea7f861bec003cd32f6fc60e8e8ed5
EduardoToro <etorob@unal.edu.co>
2022-07-14 13:53:49 -0500

    Variables

---
+++ Operadores_Aritmeticos.py
@@ -0,0 +1,18 @@
+'''
+Podemos observar los operadores aritmeticos que ofrece Python dentro de sus librerias
+'''
+
+resultado1 = 10 + 3
+
+#No es muy util colocar directamente el valor, por ende es mejor usar variables
+
+num1 = 10
+num2 = 3
+
+resultado1 = num1 + num2
+resultado2 = num1 - num2
+resultado3 = num1 * num2
+resultado4 = num1 / num2
+resultado5 = num1 // num2 #Nos da el numero redondeado a la baja
+resultado6 = num1 % num2  #Nos retorna el residuo de la division
+resultado7 = num1 ** num2 #Nos devuelve la potencia
---
+++ Variables.py
@@ -0,0 +1,40 @@
+#Guardamos los números en variables declaradas por un nombre.
+numero1 = 10
+numero2 = 10.56
+cadena1 = 'a'
+cadena2 = 'Eduardo esta "estudiando"'
+booleano1 = True
+booleano2 = False
+
+#Imprimimos lo que esta guardado en una variable asi:
+
+print(numero1)
+print(numero2)
+print(cadena1)
+print(cadena2)
+print(booleano1)
+print(booleano2)
+
+#Verificamos el tipo de valor que tienen una variable:
+
+print(type(numero1))
+print(type(numero2))
+print(type(cadena1))
+print(type(cadena2))
+print(type(booleano1))
+print(type(booleano2))
+
+#Operaciones entre variables
+suma = numero1 + numero2
+resultado = (suma*10)/5
+print("El resultado es: ", resultado)
+
+#Tipado Dinamico: Podemos cambiar el valor de una variable en cualquier momento
+
+dinamico= 10
+
+print(dinamico)
+
+dinamico = "Ewualdo"
+
+print(dinamico)
